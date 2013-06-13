8df613537272ec50fdd40492ace0688da2f8a6bb
fclamp <fc-lamp@163.com>
2013-06-13 17:38:47 +0800

    init

---
+++ src/Fclamp/Imu/ImuServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+namespace Fclamp\Imu;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImuServiceProvider extends ServiceProvider
+{
+	
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+	
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package ( 'fclamp/imu' );
+	}
+	
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app ['IMu'] = $this->app->share ( function ($app)
+		{
+			return new Imu ();
+		} );
+		
+		$this->app ['IMuSession'] = $this->app->share ( function ($app)
+		{
+			return new IMuSession ();
+		} );
+		
+		$this->app ['IMuModule'] = $this->app->share ( function ($app)
+		{
+			return new IMuModule;
+		} );
+	
+	}
+	
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array ('imu' );
+	}
+
+}
